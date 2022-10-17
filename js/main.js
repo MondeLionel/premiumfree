77c1f79ae2cc7299df030ab90d2ab042620339c1
Monde Nyawose <lionelnyawose@gmail.com>
2022-10-17 19:13:18 +0200

    [FEATURE] add scroll eventListener

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
 
 
+window.addEventListener("scroll", function(){
+  checkScroll()
+})
 
 
+  // if(document.body.scrollTop < 50 || document.documentElement.scrollTop < 50){
 
+  // }
 
-const channelSwiper = new Swiper('.swiper-container.channelSwiper', {
+const channelSwiper = new Swiper('.channelSwiper', {
   speed: 400,
-  spaceBetween: 20,
-  slidesPerView:4,
-  freeMode: true
+  spaceBetween: 10,
+  slidesPerView:2,
+  freeMode: true,
+   breakpoints: {
+    // when window width is >= 600px
+    600: {
+      slidesPerView: 4,
+      spaceBetween: 20
+    }
+  }
 });
 
 // featuredSwiper
 
-const featuredSwiper = new Swiper('.swiper-container.featuredSwiper', {
+const featuredSwiper = new Swiper('.featuredSwiper', {
   speed: 400,
   spaceBetween: 20,
   slidesPerView:1
 });
+
+
+function checkScroll(){
+   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.querySelector("body").classList.add("scrolled");
+    console.log(document.body.scrollTop)
+
+  }
+}
